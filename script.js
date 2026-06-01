334a84f1efc24a6d2c98aae11dc88c91a4af366a
cedric <cedric@gmail.com>
2026-06-01 10:57:21 +0200

    modification de code

--- script.js
+++ script.js
@@ -516,4 +516,20 @@ function showSettingPage(pageId, element) {
   element.classList.add("active");
 }
 
+function togglePassword(inputId, icon) {
+  let input = document.getElementById(inputId);
+
+  if (input.type === "password") {
+    input.type = "text";
+
+    icon.classList.remove("bi-eye");
+    icon.classList.add("bi-eye-slash");
+  } else {
+    input.type = "password";
+
+    icon.classList.remove("bi-eye-slash");
+    icon.classList.add("bi-eye");
+  }
+}
+
 changeNetworkMode();
